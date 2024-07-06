eff02c769aefef2dbb4f01ed86c6c780b9ad031c
phuongvtb <bichphuong13042002@gmail.com>
2024-07-06 21:27:22 +0700

    (Navigation bottom): Fix UI

---
+++ app-fe/src/Screens/Guest/TabNavigation/ProductManage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ProductManagementScreen = () => {
+  return (
+    <View>
+      <Text>Product Management</Text>
+    </View>
+  );
+};
+
+export default ProductManagementScreen;
